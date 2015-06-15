0bf8b0a8b167e3f90c91dde2a382a0696639cc0d
Eyog Yvon Léonce <grandeyl@gmail.com>
2015-06-15 19:14:45 +0100

    Changed tablenames and database name to lowercase

--- src/main/java/Model/CreateDB.sql
+++ src/main/java/Model/CreateDB.sql
@@ -1,10 +1,10 @@
-CREATE SCHEMA IF NOT EXISTS `Timetable` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
-USE `Timetable` ;
+CREATE SCHEMA IF NOT EXISTS `timetable` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
+USE `timetable` ;
 
 -- -----------------------------------------------------
--- Table `Timetable`.`Courses`
+-- Table `timetable`.`courses`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Timetable`.`Courses` (
+CREATE  TABLE IF NOT EXISTS `timetable`.`courses` (
   `code` VARCHAR(50) NOT NULL ,
   `Title` VARCHAR(45) NULL ,
   `semester` INT NULL ,
@@ -13,121 +13,121 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Timetable`.`Classrooms`
+-- Table `timetable`.`classrooms`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Timetable`.`Classrooms` (
-  `ID` INT NOT NULL AUTO_INCREMENT ,
+CREATE  TABLE IF NOT EXISTS `timetable`.`classrooms` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(35) NULL ,
   `capacity` INT NULL ,
-  PRIMARY KEY (`ID`) ,
+  PRIMARY KEY (`id`) ,
   UNIQUE INDEX `name_UNIQUE` (`name` ASC) )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Timetable`.`Lecturer`
+-- Table `timetable`.`lecturer`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Timetable`.`Lecturer` (
-  `ID` INT NOT NULL AUTO_INCREMENT ,
+CREATE  TABLE IF NOT EXISTS `timetable`.`lecturer` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
   `full name` VARCHAR(45) NULL ,
   `name` VARCHAR(40) NULL ,
   `password` VARCHAR(45) NULL ,
   `role` VARCHAR(30) NULL ,
-  PRIMARY KEY (`ID`) ,
+  PRIMARY KEY (`id`) ,
   UNIQUE INDEX `name_UNIQUE` (`name` ASC) )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Timetable`.`Faculty`
+-- Table `timetable`.`faculty`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Timetable`.`Faculty` (
-  `ID` INT NOT NULL AUTO_INCREMENT ,
+CREATE  TABLE IF NOT EXISTS `timetable`.`faculty` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(45) NULL ,
-  `Dean_ID` INT NOT NULL ,
-  PRIMARY KEY (`ID`, `Dean_ID`) ,
-  INDEX `fk_Faculty_Lecturer1_idx` (`Dean_ID` ASC) ,
+  `dean_id` INT NOT NULL ,
+  PRIMARY KEY (`id`, `dean_id`) ,
+  INDEX `fk_faculty_lecturer1_idx` (`dean_id` ASC) ,
   UNIQUE INDEX `name_UNIQUE` (`name` ASC) ,
-  CONSTRAINT `fk_Faculty_Lecturer1`
-    FOREIGN KEY (`Dean_ID` )
-    REFERENCES `Timetable`.`Lecturer` (`ID` )
+  CONSTRAINT `fk_faculty_lecturer1`
+    FOREIGN KEY (`dean_id` )
+    REFERENCES `timetable`.`lecturer` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Timetable`.`Departement`
+-- Table `timetable`.`departement`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Timetable`.`Departement` (
-  `ID` INT NOT NULL AUTO_INCREMENT ,
+CREATE  TABLE IF NOT EXISTS `timetable`.`departement` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(45) NULL ,
-  `Faculty_ID` INT NOT NULL ,
-  `HOD_ID` INT NOT NULL ,
-  PRIMARY KEY (`ID`) ,
-  INDEX `fk_Departement_Faculty1_idx` (`Faculty_ID` ASC) ,
-  INDEX `fk_Departement_Lecturer1` (`HOD_ID` ASC) ,
-  UNIQUE INDEX `HOD_ID_UNIQUE` (`HOD_ID` ASC) ,
-  CONSTRAINT `fk_Departement_Faculty1`
-    FOREIGN KEY (`Faculty_ID` )
-    REFERENCES `Timetable`.`Faculty` (`ID` )
+  `faculty_id` INT NOT NULL ,
+  `hod_id` INT NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_departement_faculty1_idx` (`faculty_id` ASC) ,
+  INDEX `fk_departement_lecturer1` (`hod_id` ASC) ,
+  UNIQUE INDEX `hod_id_UNIQUE` (`hod_id` ASC) ,
+  CONSTRAINT `fk_departement_faculty1`
+    FOREIGN KEY (`faculty_id` )
+    REFERENCES `timetable`.`faculty` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT `fk_Departement_Lecturer1`
-    FOREIGN KEY (`HOD_ID` )
-    REFERENCES `Timetable`.`Lecturer` (`ID` )
+  CONSTRAINT `fk_departement_lecturer1`
+    FOREIGN KEY (`hod_id` )
+    REFERENCES `timetable`.`lecturer` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Timetable`.`Departement_has_Courses`
+-- Table `timetable`.`departement_has_courses`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Timetable`.`Departement_has_Courses` (
-  `Departement_ID` INT NOT NULL ,
-  `Courses_code` VARCHAR(50) NOT NULL ,
-  PRIMARY KEY (`Departement_ID`, `Courses_code`) ,
-  INDEX `fk_Departement_has_Courses_Courses1` (`Courses_code` ASC) ,
-  INDEX `fk_Departement_has_Courses_Departement1` (`Departement_ID` ASC) ,
-  CONSTRAINT `fk_Departement_has_Courses_Departement1`
-    FOREIGN KEY (`Departement_ID` )
-    REFERENCES `Timetable`.`Departement` (`ID` )
+CREATE  TABLE IF NOT EXISTS `timetable`.`departement_has_courses` (
+  `departement_id` INT NOT NULL ,
+  `courses_code` VARCHAR(50) NOT NULL ,
+  PRIMARY KEY (`departement_id`, `courses_code`) ,
+  INDEX `fk_departement_has_courses_courses1` (`courses_code` ASC) ,
+  INDEX `fk_departement_has_courses_departement1` (`departement_id` ASC) ,
+  CONSTRAINT `fk_departement_has_courses_departement1`
+    FOREIGN KEY (`departement_id` )
+    REFERENCES `timetable`.`departement` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT `fk_Departement_has_Courses_Courses1`
-    FOREIGN KEY (`Courses_code` )
-    REFERENCES `Timetable`.`Courses` (`code` )
+  CONSTRAINT `fk_departement_has_courses_courses1`
+    FOREIGN KEY (`courses_code` )
+    REFERENCES `timetable`.`courses` (`code` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Timetable`.`Lecturer_has_Courses`
+-- Table `timetable`.`lecturer_has_courses`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Timetable`.`Lecturer_has_Courses` (
-  `Lecturer_ID` INT NOT NULL ,
-  `Courses_code` VARCHAR(50) NOT NULL ,
-  `Classrooms_ID` INT NOT NULL ,
+CREATE  TABLE IF NOT EXISTS `timetable`.`lecturer_has_courses` (
+  `lecturer_id` INT NOT NULL ,
+  `courses_code` VARCHAR(50) NOT NULL ,
+  `classrooms_id` INT NOT NULL ,
   `date` DATETIME NULL ,
-  PRIMARY KEY (`Lecturer_ID`, `Courses_code`, `Classrooms_ID`) ,
-  INDEX `fk_Lecturer_has_Courses_Courses1` (`Courses_code` ASC) ,
-  INDEX `fk_Lecturer_has_Courses_Lecturer1` (`Lecturer_ID` ASC) ,
-  INDEX `fk_Lecturer_has_Courses_Classrooms1` (`Classrooms_ID` ASC) ,
-  CONSTRAINT `fk_Lecturer_has_Courses_Lecturer1`
-    FOREIGN KEY (`Lecturer_ID` )
-    REFERENCES `Timetable`.`Lecturer` (`ID` )
+  PRIMARY KEY (`lecturer_id`, `courses_code`, `classrooms_id`) ,
+  INDEX `fk_lecturer_has_courses_courses1` (`courses_code` ASC) ,
+  INDEX `fk_lecturer_has_courses_lecturer1` (`lecturer_id` ASC) ,
+  INDEX `fk_lecturer_has_courses_classrooms1` (`classrooms_id` ASC) ,
+  CONSTRAINT `fk_lecturer_has_courses_lecturer1`
+    FOREIGN KEY (`lecturer_id` )
+    REFERENCES `timetable`.`lecturer` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT `fk_Lecturer_has_Courses_Courses1`
-    FOREIGN KEY (`Courses_code` )
-    REFERENCES `Timetable`.`Courses` (`code` )
+  CONSTRAINT `fk_lecturer_has_courses_courses1`
+    FOREIGN KEY (`courses_code` )
+    REFERENCES `timetable`.`courses` (`code` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT `fk_Lecturer_has_Courses_Classrooms1`
-    FOREIGN KEY (`Classrooms_ID` )
-    REFERENCES `Timetable`.`Classrooms` (`ID` )
+  CONSTRAINT `fk_lecturer_has_courses_classrooms1`
+    FOREIGN KEY (`classrooms_id` )
+    REFERENCES `timetable`.`classrooms` (`id` )
     ON DELETE NO ACTION
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
